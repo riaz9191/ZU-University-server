1c4084d7ca09cd94b8e70cfbd6a5e0d82302346f
riaz9191 <riazahammed9191@gmail.com>
2024-11-25 09:07:32 +0600

    bug fixing with semester yaer wise incement

--- src/app/modules/user/user.utils.ts
+++ src/app/modules/user/user.utils.ts
@@ -14,14 +14,23 @@ const findLastStudentId = async () => {
     .sort({ createdAt: -1 })
     .lean();
 
-  return lastStudent?.id ? lastStudent.id.substring(6) : undefined;
+  return lastStudent?.id ? lastStudent.id : undefined;
 };
 
 export const generateStudentId = async (payload: TAcademicSemester) => {
   //generates a 4 digit number that will be used as student id
   //padStart(4, '0') is used to add leading zeros to the number
   //for example, if the number is 1, it will be converted to '0001'
-  const currentId = (await findLastStudentId()) || (0).toString();
+  let currentId = (0).toString();
+
+  const lastStudentId = await findLastStudentId();
+  const lastStudentSemesterCode = lastStudentId?.substring(4, 6); //01
+  const lastStudentSemesterYear = lastStudentId?.substring(0, 4); //2030
+  const currentSemesterCode = payload.code;
+  const currentSemesterYear = payload.year;
+  if (lastStudentId && lastStudentSemesterCode === currentSemesterCode && lastStudentSemesterYear === currentSemesterYear) {
+    currentId = lastStudentId.substring(6);
+  }
   let incrementId = (Number(currentId) + 1).toString().padStart(4, '0');
 
   incrementId = `${payload.year}${payload.code}${incrementId}`;
