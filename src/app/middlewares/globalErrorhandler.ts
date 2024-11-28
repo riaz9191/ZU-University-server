b369598e581ff2cd081e41b6ada441ba106d90d2
riaz9191 <riazahammed9191@gmail.com>
2024-11-28 10:19:49 +0600

    hadeling zod error

--- src/app/middlewares/globalErrorhandler.ts
+++ src/app/middlewares/globalErrorhandler.ts
@@ -2,20 +2,33 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { NextFunction, Request, Response } from 'express';
+import { ErrorRequestHandler, NextFunction, Request, Response } from 'express';
+import { ZodError } from 'zod';
 
-const globalErrorHandler = (
-  err: any,
-  req: Request,
-  res: Response,
-  next: NextFunction,
-) => {
-  const statusCode = err.statusCode || 500;
-  const message = err.message || 'Something went wrong!';
+const globalErrorHandler: ErrorRequestHandler = (err, req, res, next) => {
+  //default setting value
+  let statusCode = err.statusCode || 500;
+  let message = err.message || 'Something went wrong!';
+  type TErrorSource = {
+    path: string | number;
+    message: string;
+  }[];
+  let errorSources: TErrorSource = [
+    {
+      path: '',
+      message: 'Something went wrong!',
+    },
+  ];
 
+  if (err instanceof ZodError) {
+    statusCode = 400;
+    message = 'ami zod error';
+  }
+// ultimate return
   return res.status(statusCode).json({
     success: false,
     message,
+    errorSources,
     error: err,
   });
 };
